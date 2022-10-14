c447777440fd1e538d6b0b86099299eff410d170
diddone <dmpozdeev@edu.hse.ru>
2022-10-14 22:41:41 +0300

    Remove hw_asr

--- hw_asr/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
-from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
-from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
-
-__all__ = [
-    "LibrispeechDataset",
-    "CustomDirAudioDataset",
-    "CustomAudioDataset"
-]
