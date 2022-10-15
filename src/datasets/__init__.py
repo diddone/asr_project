909dc6b39043432e58ec0a106e28d1c0d36f7f8b
diddone <dmpozdeev@edu.hse.ru>
2022-10-15 10:45:21 +0300

    Add common voice, thank you Julia

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,9 +1,11 @@
 from src.datasets.custom_audio_dataset import CustomAudioDataset
 from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.datasets.librispeech_dataset import LibrispeechDataset
+from src.datasets.common_voice_dataset import CommonVoiceDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
-    "CustomAudioDataset"
+    "CustomAudioDataset",
+    "CommonVoiceDataset",
 ]
