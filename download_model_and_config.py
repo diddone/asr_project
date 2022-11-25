1ef6dd0bf853764065fad7b77659f52903ed2c6f
diddone <80636827+diddone@users.noreply.github.com>
2022-11-25 14:02:57 +0300

    Update download_model_and_config.py

--- download_model_and_config.py
+++ download_model_and_config.py
@@ -1,7 +1,7 @@
 import gdown
 
-model_url = 'https://drive.google.com/uc?export=download&id=1VNTFKR3pydGduRYU-9zFqkjizCDp9EjL'
-config_url = 'https://drive.google.com/uc?export=download&id=14eaEuUaA2-xiMypkBxJtdM3X7Ly33BNb'
+model_url = 'https://drive.google.com/uc?export=download&id=1saBj6YJCSqVqifq645rvsy-LXBYZsN9n'
+config_url = 'https://drive.google.com/uc?export=download&id=1S7GMc26aloq-lgSwCuky42mxeWMib0Jv'
 output = 'defaul_test_model/'
 
 gdown.download(model_url, output=output)
